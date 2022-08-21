7b241aff494dcb211e041a1f59df18715304cf18
Jonathan Tolotti <jonathanstolotti@gmail.com>
2022-08-21 15:13:09 -0300

    Cria activity de login.

--- app/src/main/java/com/example/dollarapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/dollarapp/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.dollarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Chama a activity de login após a cotação do dólar
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override public void run() {
+                loadLoginActivity();
+            }
+        }, 2000);
+    }
+
+    public void loadLoginActivity() {
+        Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
