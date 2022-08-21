f75cfb88b40110777e16ed76d1136d33fa6b1417
Jonathan Tolotti <jonathanstolotti@gmail.com>
2022-08-21 15:58:59 -0300

    Adiciona estrutura para login e icones do aplicativo.

--- app/src/main/java/com/example/dollarapp/Ui/LoginActivity.java
+++ app/src/main/java/com/example/dollarapp/Ui/LoginActivity.java
@@ -3,15 +3,51 @@ package com.example.dollarapp.Ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.dollarapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private String userPhoneValue;
+    private String userPassValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * Gerencia a parte de login do aplicativo
+     */
+    public void handlerLogin(View view) {
+        createCredentialsLogin();
+
+        doLogin();
+    }
+
+    /**
+     * Busca as informações digitadas e monta as variáveis usadas pelo login
+     */
+    private void createCredentialsLogin() {
+        EditText userPhone = findViewById(R.id.LoginUserPhone);
+        EditText userPass = findViewById(R.id.LoginUserPass);
+
+        userPhoneValue = userPhone.getText().toString();
+        userPassValue = userPass.getText().toString();
+    }
+
+    /**
+     * Valida se as credenciais estão corretas e trata o retorno
+     */
+    private void doLogin() {
+        if (userPhoneValue.equals("51996766591") && userPassValue.equals("1234")) {
+            Toast.makeText(this, "Usuário correto", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Usuário e/ou senha inválido(s)", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
